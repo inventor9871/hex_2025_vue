441c0f112eb43a5d05bf70bfe12bc910bac92ac7
inventor9871 <inventor9871@gmail.com>
2025-08-01 10:42:11 +0800

    build dist/

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hex_2025_vue/',
   plugins: [
     vue(),
     vueDevTools(),
